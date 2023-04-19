5ec4ede7257566e0521a7278cb285f2f3beb36db
Bryan <bryan@hellotherma.com>
2023-04-18 23:25:19 -0700

    big change

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,155 @@
 import { useState, useEffect } from 'react';
-import { TextField, Box, Stack, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import TodoForm from './components/TodoForm';
 import axios from 'axios';
-import './App.css';
+import {
+    FormControl,
+    Select,
+    MenuItem,
+    TableContainer,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    IconButton,
+} from '@mui/material';
+import { ArrowForward } from '@mui/icons-material';
 
-type Todo = {
+interface Operation {
     id: string;
-    title: string;
-    description: string;
-    priority: string;
-    dueDate: string;
-};
+    name: string;
+    duration: number;
+}
 
-const App = () => {
-    const [todos, setTodos] = useState<Todo[]>([]);
-    const [searchQuery, setSearchQuery] = useState('');
+interface Dependency {
+    from: string;
+    to: string;
+}
 
-    const columns: GridColDef[] = [
-        { field: 'title', headerName: 'Title', width: 150 },
-        { field: 'description', headerName: 'Description', width: 250 },
-        { field: 'priority', headerName: 'Priority', width: 100 },
-        {
-            field: 'dueDate',
-            headerName: 'Due Date',
-            width: 250,
-            valueFormatter: (params) => {
-                const date = new Date(params.value);
-                const dateHumanReadable = date.toDateString();
-                return dateHumanReadable;
-            },
-        },
-        {
-            field: 'delete',
-            headerName: '',
-            sortable: false,
-            width: 50,
-            renderCell: (params) => (
-                <IconButton onClick={() => handleDelete(params)}>
-                    <DeleteIcon />
-                </IconButton>
-            ),
-        },
-    ];
+interface Job {
+    jobId: string;
+    operations: Operation[];
+    dependencies: Dependency[];
+    criticalPath: Operation[];
+    currentOperationIndex: number;
+}
 
-    const handleDelete = async (params: any) => {
-        await axios.delete(`http://localhost:3000/todos/${params.row.id}`);
-        fetchTodos();
+const App: React.FC = () => {
+    const [jobs, setJobs] = useState<Job[]>([]);
+    const [selectedJob, setSelectedJob] = useState<Job | undefined>(undefined);
+
+    useEffect(() => {
+        // Replace this with the actual API call to get the job data
+        async function fetchJobs() {
+            try {
+                // Replace the URL with your API endpoint
+                const response = await axios.get<Job[]>(
+                    'http://localhost:3000/jobs',
+                );
+                console.log('data', response.data);
+                setJobs(response.data.data);
+            } catch (error) {
+                console.error('Error fetching job data:', error);
+            }
+        }
+
+        fetchJobs();
+    }, []);
+
+    const handleSelectChange = (
+        event: React.ChangeEvent<{ value: unknown }>,
+    ) => {
+        const jobId = event.target.value as string;
+        setSelectedJob(jobs.find((job) => job.jobId === jobId));
     };
 
-    const fetchTodos = async (query = '') => {
-        try {
-            let response;
-            if (query.length > 0) {
-                response = await axios.get(
-                    `http://localhost:3000/todos/query/${query}`,
-                );
-            } else {
-                response = await axios.get(`http://localhost:3000/todos/`);
-            }
-
-            if (response && response.data.data.todos.length > 0) {
-                setTodos(response.data.data.todos);
-            }
-        } catch (error) {
-            console.error(error);
+    const handleJobClick = (job: Job) => {
+        if (job.currentOperationIndex < job.operations.length - 1) {
+            job.currentOperationIndex += 1;
+            setSelectedJob({ ...job });
         }
     };
 
-    // Debouncer
-    useEffect(() => {
-        const getData = setTimeout(() => {
-            fetchTodos(searchQuery);
-        }, 1000);
-
-        return () => clearTimeout(getData);
-    }, [searchQuery]);
-
-    useEffect(() => {
-        fetchTodos();
-    }, []);
-
     return (
-        <>
-            <Box sx={{ width: '60%' }}>
-                <Stack spacing={2}>
-                    <TodoForm fetchTodos={fetchTodos} />
-                    <TextField
-                        label="Search"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        sx={{ marginBottom: '1rem' }}
-                    />
-                    <DataGrid rows={todos} columns={columns} autoHeight />
-                </Stack>
-            </Box>
-        </>
+        <div>
+            <h1>Job Data</h1>
+            <FormControl variant="outlined" sx={{ minWidth: 200 }}>
+                <Select
+                    value={selectedJob?.jobId || ''}
+                    onChange={handleSelectChange}
+                    displayEmpty
+                >
+                    <MenuItem value="" disabled>
+                        Select a job
+                    </MenuItem>
+                    {jobs.map((job) => (
+                        <MenuItem key={job.jobId} value={job.jobId}>
+                            {job.jobId}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            {selectedJob && (
+                <TableContainer component={Paper} sx={{ mt: 2 }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Current Operation</TableCell>
+                                <TableCell>Current Job Duration</TableCell>
+                                <TableCell>Select Next Operation</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    {
+                                        selectedJob.operations[
+                                            selectedJob.currentOperationIndex
+                                        ].name
+                                    }
+                                </TableCell>
+                                <TableCell>
+                                    {
+                                        selectedJob.operations[
+                                            selectedJob.currentOperationIndex
+                                        ].duration
+                                    }
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton
+                                        onClick={() =>
+                                            handleJobClick(selectedJob)
+                                        }
+                                    >
+                                        <ArrowForward />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={3}>
+                                    Upcoming Operations:
+                                </TableCell>
+                            </TableRow>
+                            {selectedJob.operations.map((op, index) => (
+                                <TableRow key={op.id}>
+                                    <TableCell
+                                        sx={{
+                                            bgcolor:
+                                                index ===
+                                                selectedJob.currentOperationIndex
+                                                    ? '#C7F2E2'
+                                                    : 'inherit',
+                                        }}
+                                    >
+                                        {op.name} ({op.duration})
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
+        </div>
     );
 };
 
